8443a8a4a3f637c203ae45bb3ec25d9be38e5c69
HBMY289 <HBMY289@gmail.com>
2020-09-01 15:52:51 +0200

    try verifying word list

--- main.go
+++ main.go
@@ -2,36 +2,85 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
 	"time"
 
-	// "github.com/dongri/go-mnemonic"
 	"github.com/iotaledger/iota.go/address"
 	"github.com/iotaledger/iota.go/kerl"
 	"github.com/tyler-smith/go-bip32"
 	"github.com/tyler-smith/go-bip39"
+	"github.com/tyler-smith/go-bip44"
 )
 
+var wg = sync.WaitGroup{}
+
 func main() {
-
-	// var bip44AccountIndex uint32 = 0x00000000
-	var bip44PageIndex uint32 = 0x00000000
-
+	getInputs()
 	mnemonic := "witch collapse practice feed shame open despair creek road again ice least witch collapse practice feed shame open despair creek road again ice least"
-	seed := ""
-	// var addrs = []string{}
+	//targetAddr := "WNMNYMAOBGWPNFYZHSQHNXMOOIURFWAZUVVCNVZCKNKBH9XOGWUPFRWPSFAHBMMMZKXDJJGIOTERPSEUB" // addindex #3 accountindex #8
+	targetAddr := "SZE9WDWHUUYGOXQRMZWKHFHSQCVU9NROSNFERAJMT9YFIHHRCKRFSDESFWDPCLPMJFFXLXZISLWKBSKTC" // addindex #8 acc index #98
 	startTime := time.Now()
-	for i := 0; i < 100; i++ {
-		seed = mnemonicToSeed(mnemonic, uint32(i), bip44PageIndex)
-		//addrs = getAddrsOfSeed(seed, 10)
-	}
+	startSearch(mnemonic, targetAddr, 50, 0, 100, 0, 0)
 	endTime := time.Now()
-	fmt.Println("Seed: ", seed)
-	// fmt.Println(addrs)
 	fmt.Println(endTime.Sub(startTime))
+	// seed := ""
+	// var addrs = []string{}
+	// startTime := time.Now()
+	// for i := 0; i < 200; i++ {
+	// 	seed = mnemonicToSeed(mnemonic, uint32(i), pageIndex)
+	// 	addrs = getAddrsOfSeed(seed, 10)
+	// }
+	// endTime := time.Now()
+	// fmt.Println("Seed: ", seed)
+	// fmt.Println(addrs)
+	// fmt.Println(endTime.Sub(startTime))
 
 }
 
+func startSearch(mnemonic, targetAddr string, addrsPerSeed int, accIndexStart, accIndexEnd, pageIndexStart, pageIndexEnd uint32) {
+
+	seedChan := make(chan mySeed, 100)
+
+	workers := runtime.GOMAXPROCS(-1)
+	wg.Add(1)
+	go generateSeeds(mnemonic, seedChan, accIndexStart, accIndexEnd)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go checkAddresses(targetAddr, seedChan, addrsPerSeed)
+	}
+	go checkAddresses(targetAddr, seedChan, addrsPerSeed)
+	fmt.Println("start searching")
+	wg.Wait()
+	fmt.Println("finished searching")
+}
+
+func generateSeeds(mnemonic string, seedChan chan mySeed, accIndexStart, accIndexEnd uint32) {
+	for i := accIndexStart; i <= accIndexEnd; i++ {
+		seed := mySeed{mnemonicToSeed(mnemonic, i, 0), i, 0}
+		seedChan <- seed
+		fmt.Println(len(seedChan))
+	}
+	close(seedChan)
+	wg.Done()
+}
+
+func checkAddresses(targetAddr string, seedChan chan mySeed, addrsPerSeed int) {
+	for seed := range seedChan {
+		addrs := getAddrsOfSeed(seed.string, addrsPerSeed)
+		// fmt.Println(addrs[8])
+		for _, addr := range addrs {
+			if strings.Contains(addr, targetAddr) {
+				fmt.Printf("found match for account: %d\n", seed.accountIndex)
+			}
+		}
+	}
+	wg.Done()
+}
 func mnemonicToSeed(mnemonic string, accountIndex, pageIndex uint32) string {
 	seed := bip39.NewSeed(mnemonic, "")
 	bip32RootKey, _ := bip32.NewMasterKey(seed)
@@ -50,7 +99,43 @@ func mnemonicToSeed(mnemonic string, accountIndex, pageIndex uint32) string {
 	return trytes
 }
 
+func getInputs() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	mnemonic := getMnemonic(scanner)
+	return mnemonic
+}
+
+func getMnemonic(scanner *bufio.Scanner) string {
+	var words [23]string
+	i := 1
+	for i <= 24 {
+		fmt.Printf("Enter mnemonic word #%d:", i)
+		scanner.Scan()
+		word := scanner.Text()
+		fmt.Println()
+		if isValidWord(word) {
+			words[i] = word
+			i++
+
+		} else {
+			fmt.Printf("'%v' is not a valid BIP 44 word. Try again.\n", word)
+		}
+
+	}
+	return strings.Join(words[:], " ")
+}
+
+func isValidWord(word string) bool {
+	_, valid := bip39.GetWordIndex(word)
+	return valid
+}
+
 func getAddrsOfSeed(seed string, addCount int) []string {
 	addrs, _ := address.GenerateAddresses(seed, 0, uint64(addCount), 2, false)
 	return addrs
+}
+
+type mySeed struct {
+	string                  string
+	accountIndex, pageIndex uint32
 }
