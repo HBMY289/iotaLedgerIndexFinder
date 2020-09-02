5f2bd896ceefe17c71158dcb5098ee97d445f3ab
HBMY289 <HBMY289@gmail.com>
2020-09-02 16:15:31 +0200

    added status and result msgs

--- main.go
+++ main.go
@@ -21,74 +21,84 @@ import (
 var wg = sync.WaitGroup{}
 
 func main() {
-	_, targetAddr := getInputs()
+	// mnemonic, targetAddr := getInputs()
 	mnemonic := "wheel mosquito enroll illness stamp vote tomorrow mandate powder armed fortune buffalo rack mirror elder fun paper between cheap present vast unlock detect birth" //targetAddr := "WNMNYMAOBGWPNFYZHSQHNXMOOIURFWAZUVVCNVZCKNKBH9XOGWUPFRWPSFAHBMMMZKXDJJGIOTERPSEUB" // addindex #3 accountindex #8
-	//targetAddr := "SZE9WDWHUUYGOXQRMZWKHFHSQCVU9NROSNFERAJMT9YFIHHRCKRFSDESFWDPCLPMJFFXLXZISLWKBSKTC" // addindex #8 acc index #98
-	//targetAddr := "CRICOFALQY9XBDSPOJAID9TMKMUNYWVN99WEUFOTCNBYZCNALGUCDDMQTHYWZVFMNWBYGBBBDUWKJPAFZCCCKMCBAX" addindex #1 accindex 9
+	// targetAddr := "SZE9WDWHUUYGOXQRMZWKHFHSQCVU9NROSNFERAJMT9YFIHHRCKRFSDESFWDPCLPMJFFXLXZISLWKBSKTC"                                                                               // addindex #8 acc index #98
+	// targetAddr := "CRICOFALQY9XBDSPOJAID9TMKMUNYWVN99WEUFOTCNBYZCNALGUCDDMQTHYWZVFMNWBYGBBBDUWKJPAFZ" //addindex #1 accindex 9
+	targetAddr := "JWTWV9KLWZRORTCQGBHEYZFQLZUIGLGJASFDGQOKAVSYIBKOGONQDZZTLM9IYE9GVBTPBSXEWLIDBQYF9_" //addindex #2 accountindex 99
+	accIndexStart := 0
+	accIndexEnd := 200
+	addrsPerSeed := 10
+	pageIndexStart := 0
+	pageIndexEnd := 0
 	startTime := time.Now()
-	startSearch(mnemonic, targetAddr, 50, 0, 100, 0, 0)
-	endTime := time.Now()
-	fmt.Println(endTime.Sub(startTime))
+	matchedIndex := getMatchingIndex(mnemonic, targetAddr, addrsPerSeed, accIndexStart, accIndexEnd, pageIndexStart, pageIndexEnd)
+	deltaT := time.Now().Sub(startTime)
 
-	// seed := ""
-	// var addrs = []string{}
-	// startTime := time.Now()
-	// for i := 0; i < 200; i++ {
-	// 	seed = mnemonicToSeed(mnemonic, uint32(i), pageIndex)
-	// 	addrs = getAddrsOfSeed(seed, 10)
-	// }
-	// endTime := time.Now()
-	// fmt.Println("Seed: ", seed)
-	// fmt.Println(addrs)
-	// fmt.Println(endTime.Sub(startTime))
+	if matchedIndex >= 0 {
+		fmt.Printf("\nFound matching address for account index '%d' after %v.", matchedIndex, deltaT.Round(time.Second))
+		return
+	}
+	fmt.Printf("\nCould not find a match after %v testing the first %d addresses of indexes %d to %d.\nCheck target address or retry with larger values for maximum index and addresses per seed.", deltaT.Round(time.Second), addrsPerSeed, accIndexStart, accIndexEnd)
 
 }
 
-func startSearch(mnemonic, targetAddr string, addrsPerSeed int, accIndexStart, accIndexEnd, pageIndexStart, pageIndexEnd uint32) {
+func getMatchingIndex(mnemonic, targetAddr string, addrsPerSeed, accIndexStart, accIndexEnd, pageIndexStart, pageIndexEnd int) int {
 
-	seedChan := make(chan mySeed, 100)
-
+	seedChan := make(chan mySeed)
+	stopChan := make(chan struct{}, 1)
 	workers := runtime.GOMAXPROCS(-1)
+	matchedIndex := -1
 	wg.Add(1)
-	go generateSeeds(mnemonic, seedChan, accIndexStart, accIndexEnd)
+	go generateSeeds(mnemonic, seedChan, accIndexStart, accIndexEnd, stopChan)
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go checkAddresses(targetAddr, seedChan, addrsPerSeed)
+		go checkAddresses(targetAddr, seedChan, addrsPerSeed, &matchedIndex, stopChan)
 	}
-	go checkAddresses(targetAddr, seedChan, addrsPerSeed)
 	fmt.Println("start searching")
 	wg.Wait()
-	fmt.Println("finished searching")
+	fmt.Println("\nstopped searching")
+	return matchedIndex
+
 }
 
-func generateSeeds(mnemonic string, seedChan chan mySeed, accIndexStart, accIndexEnd uint32) {
+func generateSeeds(mnemonic string, seedChan chan<- mySeed, accIndexStart, accIndexEnd int, stopChan <-chan struct{}) {
+	defer close(seedChan)
+	defer wg.Done()
 	for i := accIndexStart; i <= accIndexEnd; i++ {
+		select {
+		case <-stopChan:
+			return
+		default:
+		}
 		seed := mySeed{mnemonicToSeed(mnemonic, i, 0), i, 0}
 		seedChan <- seed
 	}
-	close(seedChan)
-	wg.Done()
 }
 
-func checkAddresses(targetAddr string, seedChan chan mySeed, addrsPerSeed int) {
+func checkAddresses(targetAddr string, seedChan <-chan mySeed, addrsPerSeed int, matchedIndex *int, stopChan chan<- struct{}) {
 	for seed := range seedChan {
 		addrs := getAddrsOfSeed(seed.string, addrsPerSeed)
-		// fmt.Println(addrs[8])
+		if seed.accountIndex%10 == 0 {
+			fmt.Printf("\rchecking index #%d", seed.accountIndex)
+		}
 		for _, addr := range addrs {
 			if strings.Contains(addr, targetAddr) {
-				fmt.Printf("found match for account: %d\n", seed.accountIndex)
+				*matchedIndex = int(seed.accountIndex)
+				stopChan <- struct{}{}
 			}
 		}
 	}
 	wg.Done()
 }
-func mnemonicToSeed(mnemonic string, accountIndex, pageIndex uint32) string {
+
+func mnemonicToSeed(mnemonic string, accountIndex, pageIndex int) string {
 	seed := bip39.NewSeed(mnemonic, "")
 	bip32RootKey, _ := bip32.NewMasterKey(seed)
 	bip44PuposeKey, _ := bip32RootKey.NewChildKey(0x8000002C)
 	bip44CoinTypeKey, _ := bip44PuposeKey.NewChildKey(0x8000107A)
-	bip44AccountKey, _ := bip44CoinTypeKey.NewChildKey(0x80000000 + accountIndex)
-	bip44PageIndexKey, _ := bip44AccountKey.NewChildKey(0x80000000 + pageIndex)
+	bip44AccountKey, _ := bip44CoinTypeKey.NewChildKey(0x80000000 + uint32(accountIndex))
+	bip44PageIndexKey, _ := bip44AccountKey.NewChildKey(0x80000000 + uint32(pageIndex))
 	privateKey := bip44PageIndexKey.Key
 	chainCode := bip44PageIndexKey.ChainCode
 	kerlInput := append(privateKey[0:32], chainCode[0:16]...)
@@ -102,10 +112,11 @@ func mnemonicToSeed(mnemonic string, accountIndex, pageIndex uint32) string {
 
 func getInputs() (string, string) {
 	scanner := bufio.NewScanner(os.Stdin)
-	//mnemonic := getMnemonic(scanner)
+	mnemonic := getMnemonic(scanner)
 	addr := getTargetAddress(scanner)
-	return "", addr
+	return mnemonic, addr
 }
+
 func getTargetAddress(scanner *bufio.Scanner) string {
 
 	var addr string
@@ -122,6 +133,7 @@ func getTargetAddress(scanner *bufio.Scanner) string {
 
 	return addr[0:81]
 }
+
 func getMnemonic(scanner *bufio.Scanner) string {
 	var words [24]string
 	i := 1
@@ -154,5 +166,5 @@ func getAddrsOfSeed(seed string, addCount int) []string {
 
 type mySeed struct {
 	string                  string
-	accountIndex, pageIndex uint32
+	accountIndex, pageIndex int
 }
